2eb2643bc057895e04c51bf5e3755fc46be143ff
Ole Schwagmann <syentixdev@protonmail.com>
2020-09-24 12:54:26 +0200

    It parses the given hardcoded file.

--- Lesson-C.md
+++
@@ -1,3 +0,0 @@
-## Hello /j"konnichiwa"!
-
-This is a sample file for converting /j"nihongo" stuff to hiragana or katakana.
--- main.go
+++ main.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"regexp"
+
+	"github.com/gojp/kana"
 )
 
+// Used Regex for files
 var reg = regexp.MustCompile("/j\"[a-zA-Z ]+\"")
 
 func main() {
-	file, err := ioutil.ReadFile("Lesson-C.md")
+
+	// TODO: Dehardcode this bit
+	fileName := "./examples/Lesson-C"
+
+	// Read file
+	file, err := ioutil.ReadFile(fileName + ".md")
 	if err != nil {
 		panic(err)
 	}
-	// Go line by line
-	// Take the array of found matches
-	// Translate and then replace
-	first := reg.Match(file)
 
-	fmt.Println(first)
+	// How many matches are in the Text.
+	occurs := reg.FindAllString(string(file), len(file))
+	fmt.Printf("There were %d matches found.\n", len(occurs))
+
+	// Loop through the matches and replacing the text with Hiragana
+	i := 0
+	for i < len(occurs) {
+		// Gives starting and ending index
+		occur := reg.FindIndex(file)
+		start := occur[0]
+		end := occur[1]
+
+		// Converting to hiragana
+		word := kana.RomajiToHiragana(string(file[start:end]))
+
+		// Replacing placeholder with parsed text with form: <Hiragana>(<Romaji>)
+		word = word[3:len(word)-1] + "(" + string(file[start+3:end-1]) + ")"
+		file = bytes.Replace(file, file[start:end], []byte(word), len([]byte(word)))
+		i++
+	}
+
+	newFile, err := os.Create(fileName + "(parsed).md")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = newFile.Write(file)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("File successfully parsed and can be found here:", fileName+"(parsed).md")
 }
